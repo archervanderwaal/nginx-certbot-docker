60674f5637c2c2f293bf2253748aa79f72896c84
archer <archer.vanderwaal@gmail.com>
2019-03-10 17:13:16 +0800

    Add renew.sh script to renew the certificates already obtained

--- renew.sh
+++ renew.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-domains=(`awk -F '=' '/domains/ {gsub(/[()]+/,"",$2); print $2}' config.env`)
-email=`awk -F '=' '/email/ {print $2}' config.env`
-data_path=`awk -F '=' '/data_path/ {print $2}' config.env`
-rsa_key_size=`awk -F '=' '/rsa_key_size/ {print $2}' config.env`
-staging=`awk -F '=' '/staging/ {print $2}' config.env`
+# Renew the certificates already obtained
+docker-compose run --rm --entrypoint "\
+	certbot renew" certbot
 
+# Reload certificates with nginx -s reload
+docker-compose exec nginx nginx -s reload
